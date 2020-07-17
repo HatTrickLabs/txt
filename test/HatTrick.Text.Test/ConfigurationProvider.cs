02c004db8d19d7536b85a1c95562112d3666207c
Gary Grubbs <gary.grubbs@gmail.com>
2020-07-17 15:36:23 -0500

    Added unit test project and unit test pattern.

---
+++ test/HatTrick.Text.Test/ConfigurationProvider.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace HatTrick.Text.Test
+{
+    public static class ConfigurationProvider
+    {
+        public static IConfiguration Configuration { get; private set; }
+        public static string InputPath { get; private set; }
+        public static string OutputPath { get; private set; }
+
+        static ConfigurationProvider()
+        {
+            Configuration = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
+                    .AddEnvironmentVariables()
+                    .Build();
+
+            InputPath = Path.Combine(GetCurrentPath(), "..\\..\\", Configuration.GetValue("inPath", string.Empty));
+            OutputPath = Path.Combine(GetCurrentPath(), "..\\..\\", Configuration.GetValue("outPath", string.Empty));
+        }
+
+        private static string GetCurrentPath()
+        {
+            var exePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase);
+            var appPathMatcher = new Regex(@"(?<!fil)[A-Za-z]:\\+[\S\s]*?(?=\\+bin)");
+            var appRoot = appPathMatcher.Match(exePath).Value;
+            return appRoot;
+        }
+    }
+}
